de05dede2cbff7264cb993e8b41a0feb2cbc56cd
Jesus Aranda <aranda.jm@icloud.com>
2020-03-17 16:38:13 -0300

    install redux and create the application store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { applyMiddleware, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a redux store
+const store = createStore(reducer, applyMiddleware(createLogger()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
